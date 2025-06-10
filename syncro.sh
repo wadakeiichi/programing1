842fc7ebe6aff1ef7cdaa376e78be9623d7e2459
Keiichi Wada <wada@astrophysics.jp>
2025-06-10 04:48:49 +0000

    syncronize script

---
+++ syncro.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "🧪 .devcontainer の自己更新チェック..."
+
+if git fetch origin &>/dev/null; then
+  echo "リモートに接続成功。最新の .devcontainer を取得します。"
+  rm -rf .devcontainer
+  git checkout origin/main -- .devcontainer
+  echo "更新完了"
+  make -f makefile update-devcontainer
+else
+  echo "リモートに接続できません。スキップします。"
+fi
