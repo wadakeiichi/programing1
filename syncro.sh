c8c1621eb01f2ce640ee3ad74b3562b6cca1c827
Keiichi Wada <wada@astrophysics.jp>
2025-06-16 23:43:19 +0000

    メッセージ更新

--- syncro.sh
+++ syncro.sh
@@ -7,8 +7,9 @@ if git fetch origin &>/dev/null; then
   echo "リモートに接続成功。最新の .devcontainer を取得します。"
   rm -rf .devcontainer
   git checkout origin/main -- .devcontainer
-  echo "更新完了"
+  echo " "
   make -f makefile update-devcontainer
+  echo "更新完了. 左下の><開発コンテナー、アイコンから「コンテナーのリビルド」を選択してください。"
 else
   echo "リモートに接続できません。スキップします。"
 fi
